2d069286f22aba865c29cab60dcf22befd1b4535
Yukina <JustSweetLuna@gmail.com>
2020-09-09 23:57:58 +0200

    restart command fix

--- cmds/components/restart.js
+++ cmds/components/restart.js
@@ -1,4 +1,6 @@
-const exec = require('child_process').exec
+const {
+  token
+} = require('./../../settings.json')
 const Discord = require('discord.js')
 
 module.exports = {
@@ -17,7 +19,6 @@ module.exports = {
       .setTitle(':gear:**Performing Restart**:gear: ')
       .setThumbnail('https://camo.githubusercontent.com/03757a303777b6b2908fb8a64563d9c68d26ebfb/68747470733a2f2f692e696d6775722e636f6d2f4e3172374832472e706e67')
     message.channel.send(Restart).then(console.clear())
-    console.log('Performing Restart')
-    process.exit(0);
+    yukii.destroy().then(yukii.login(token))
   }
 }
